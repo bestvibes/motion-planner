cb2ed66d82a85a831384d135dfa4024e681f3260
Tao Gao <taogao.pys@gmail.com>
2017-12-05 13:54:56 -0500

    g and g_jac works

--- direct_collection.py
+++ direct_collection.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sp
 import itertools as it
 import pyipopt as ip
+import itertools as it
 
 def get_point_index(t, qdim, udim):
     # qdim, udim = self.qdim, self.udim
@@ -12,12 +13,14 @@ def get_point_index(t, qdim, udim):
     ut_index = [vt_index[-1], vt_index[-1]+udim]
     return qt_index, vt_index, ut_index
 
+
 def get_point_q_v_u(traj, t, qdim, udim):
     q_index, v_index, u_index = get_point_index(t, qdim, udim)
     q = traj[q_index[0]:q_index[-1]]
     v = traj[v_index[0]:v_index[-1]]
     u = traj[u_index[0]:u_index[-1]]
     return q, v, u
+
 
 def block_dymamics(q, v, u):
     return u
@@ -39,10 +42,10 @@ class Point_constraint():
 
 
 class Point_Dynamic_Error():
-    def __init__(self, index, problem, dynamics):
-        self.t = index
+    def __init__(self, problem, dynamics, t):
         self.prob = problem
         self.dynamics = dynamics
+        self.t = t
 
     def __call__(self, traj):
         #return a vector, representing the dynamic error at time i
@@ -57,14 +60,13 @@ class Point_Dynamic_Error():
         #TODO: each point is evaluted twice; this can be improved
         d0 = np.hstack([v0, self.dynamics(q0, v0, u0)])
         d1 = np.hstack([v1, self.dynamics(q1, v1, u1)])
-
         error = (x1 - x0) - 0.5*self.prob["dt"]*(d0 + d1) #3.2 of Kelly(2017)
         return error
 
 
-class Point_Dynamic_Error_Jacobian():
-    def __init__(self, index, problem, dynamics_jac):
-        self.t = index
+class Point_Dynamic_Jacobian():
+    def __init__(self, problem, dynamics_jac, t):
+        self.t = t
         self.prob = problem
         # dynamics_jac evaluats the jacobian of accelation = f(q, v, u)
         self.dynamics_jac = dynamics_jac
@@ -74,26 +76,68 @@ class Point_Dynamic_Error_Jacobian():
         self.dim_q_jac_lst = [-1, -0.5*self.prob["dt"], 1, -0.5*self.prob["dt"]]
         self.q_jac_lst = self.dim_q_jac_lst * self.prob['qdim']
 
-    def __call__(traj):
-        #returns an array with shape (2*qdim, ||traj||)
+        #the position of non-zero jacobian with shape(2*qdim, ||traj||)
+        self.position = self.get_position()
+
+
+    def get_position(self):
+        dq_position = self.get_jq_position()
+        dv_position = self.get_jv_position()
+        position = dq_position + dv_position
+        return position
+
+
+    def get_jq_position(self):
+        pdim = 2 * self.prob["qdim"] + self.prob["udim"]
+        qdim = self.prob["qdim"]
+        t = self.t
+        def get_dim_jac_position(t, d):
+            q0_i = pdim*t + d
+            q1_i = pdim*(t+1) + d
+            v0_i = q0_i + qdim
+            v1_i = q1_i + qdim
+            return [q0_i, v0_i, q1_i, v1_i]
+
+        q_position_lst = [get_dim_jac_position(t, d) for d in range(qdim)]
+        return q_position_lst
+
+    def get_jv_position(self):
+        qdim = self.prob["qdim"]
+        udim = self.prob["udim"]
+        t = self.t
+        q0_index, v0_index, u0_index = get_point_index(t, qdim, udim)
+        q1_index, v1_index, u1_index = get_point_index(t+1, qdim, udim)
+        row_v_position = list(range(q0_index[0], u1_index[-1]))#TODO: check end point
+        # v_position = row_v_position*qdim
+        v_position = [row_v_position for d in range(qdim)]
+        return v_position
+
+
+    def value(self, traj):
+        #TODO: check this function !!
+        #values of non-zero jacobian; the full jacobian is (2*qdim, ||traj||)
         #the jacobian of the positon error is determined by postion and  velocity
         #And it is not influenced by the dynamic_jac, which is associated with acc
         # q_jac_lst = [-1, -0.5*self.prob["dt"], 1, -0.5*self.prob["dt"]] * self.qdim
         # postion derivative
+        t = self.t
+        dt = self.prob["dt"]
+        qdim = self.prob["qdim"]
+        udim = self.prob["udim"]
 
-        q0, v0, u0 = get_point_q_v_u(traj, self.t, self.prob["qdim"], self.prob["udim"])
-        q1, v1, u1 = get_point_q_v_u(traj, self.t+1, self.prob["qdim"], self.prob["udim"])
+        q0, v0, u0 = get_point_q_v_u(traj, t, qdim, udim)
+        q1, v1, u1 = get_point_q_v_u(traj, t+1, qdim, udim)
 
         a0_jac = self.dynamics_jac(q0, v0, u0)
         a1_jac = self.dynamics_jac(q1, v1, u1)
 
-        dq0 = -0.5*self.prob["dt"]*a0_jac[:,0:self.qdim]
-        dv0 = -1 - 0.5*self.prob["dt"]*a0_jac[:, self.qdim:2*self.qdim]
-        du0 = -0.5*self.prob["dt"]*a0_jac[:, 2*self.qdim:2*self.qdim+self.udim]
+        dq0 = -0.5*dt*a0_jac[:,0:qdim]
+        dv0 = -1 - 0.5*dt*a0_jac[:, qdim:2*qdim]
+        du0 = -0.5*dt*a0_jac[:, 2*qdim:2*qdim+udim]
 
-        dq1 = -0.5*self.prob["dt"]*a1_jac[:,0:self.qdim]
-        dv1 = 1 - 0.5*self.prob["dt"]*a1_jac[:, self.qdim:2*self.qdim]
-        du1 = -0.5*self.prob["dt"]*a1_jac[:, 2*self.qdim:2*self.qdim+self.udim]
+        dq1 = -0.5*dt*a1_jac[:,0:qdim]
+        dv1 = 1 - 0.5*dt*a1_jac[:, qdim:2*qdim]
+        du1 = -0.5*dt*a1_jac[:, 2*qdim:2*qdim+udim]
 
         v_jac = np.concatenate([dq0, dv0, du0, dq1, dv1, du1], axis=1)
         v_jac_flat = v_jac.flatten()
@@ -102,154 +146,107 @@ class Point_Dynamic_Error_Jacobian():
         return point_jac_flat
 
 
-class Dynamic_constraints():
-    def __init__(self, qdim, udim, dt, n, dynamics, dynamics_g=None):
-        self.qdim = qdim
-        self.udim = udim
-        self.dynamics = dynamics
-        self.dynamics_jac = dynamics_g
-        self.dt = dt
-        self.n = n
-        self.point_dim = self.qdim*2+self.udim
-        self.size = (self.point_dim)*self.n
-        self.create_jacobian_mask()
-
-    def create_jacobian_mask(self):
-        jac_row_lst = [self.get_point_jac_mask(t) for t in range(0, self.n-1)]
-        self.jac_mask = np.vstack(jac_row_lst)
-        self.jac_index = np.where(self.jac_mask==True)
-
-    def get_point_jac_mask(self, t):
-        q_jac = self.get_point_q_jac_mask(t)
-        v_jac = self.get_point_v_jac_mask(t)
-        point_jac_mask = np.vstack([q_jac, v_jac])
-        return point_jac_mask
-
-    def get_point_q_jac_mask(self, t):
-        q_jac_lst = [self.get_point_q_dim_jac_mask(t, i)
-                     for i in range(self.qdim)]
-        q_jac = np.vstack(q_jac_lst)
-        return q_jac
-
-
-    def get_point_q_dim_jac_mask(self, t, i):
-        q0_i = (self.point_dim)*t + i
-        q1_i = (self.point_dim)*(t+1) + i
-        v0_i = q0_i + self.qdim
-        v1_i = q1_i + self.qdim
-        q_dim_jac = np.zeros(self.size, dtype=bool)
-        q_dim_jac[[q0_i, q1_i, v0_i, v1_i]] = True
-        return q_dim_jac
-
-
-    def get_point_v_jac_mask(self, t):
-        v_jac = np.zeros((self.qdim, self.size), dtype=bool)
-        q0_index, v0_index, u0_index = get_point_index(t, self.qdim, self.udim)
-        q1_index, v1_index, u1_index = get_point_index(t+1, self.qdim, self.udim)
-        v_jac[:, q0_index[0]:u1_index[-1]] = True
-        return v_jac
-
-
-    def get_point_jac_value(self, traj, t):
-        q_jac_lst = [-1, -0.5*self.dt, 1, -0.5*self.dt] * self.qdim
-        # postion derivative
-
-        q0_index, v0_index, u0_index = get_point_index(t, self.qdim, self.udim)
-        q1_index, v1_index, u1_index = get_point_index(t+1, self.qdim, self.udim)
-        q0 = traj[q0_index[0]:q0_index[-1]]
-        v0 = traj[v0_index[0]:v0_index[-1]]
-        u0 = traj[u0_index[0]:u0_index[-1]]
-        q1 = traj[q1_index[0]:q1_index[-1]]
-        v1 = traj[v1_index[0]:v1_index[-1]]
-        u1 = traj[u1_index[0]:u1_index[-1]]
-
-        a0_jac = self.dynamics_jac(q0, v0, u0)
-        a1_jac = self.dynamics_jac(q1, v1, u1)
-
-        dq0 = -0.5*self.dt*a0_jac[:,0:self.qdim]
-        dv0 = -1 - 0.5*self.dt*a0_jac[:, self.qdim:2*self.qdim]
-        du0 = -0.5*self.dt*a0_jac[:, 2*self.qdim:2*self.qdim+self.udim]
-
-        dq1 = -0.5*self.dt*a1_jac[:,0:self.qdim]
-        dv1 = 1 - 0.5*self.dt*a1_jac[:, self.qdim:2*self.qdim]
-        du1 = -0.5*self.dt*a1_jac[:, 2*self.qdim:2*self.qdim+self.udim]
-
-        v_jac = np.concatenate([dq0, dv0, du0, dq1, dv1, du1], axis=1)
-        v_jac_flat = v_jac.flatten()
-
-        point_jac_flat = np.hstack([q_jac_lst, v_jac_flat])
-        return point_jac_flat
-
-
-    def get_keypoints_index(self, t):
-        qdim, udim = self.qdim, self.udim
-        start = t*self.point_dim
-        qt_index = [start, start+qdim]
-        vt_index = [qt_index[-1], qt_index[-1]+qdim]
-        ut_index = [vt_index[-1], vt_index[-1]+udim]
-        return qt_index, vt_index, ut_index
-
-
-    def dynamic_error(self, t, traj):
-        q0_index, v0_index, u0_index = self.get_keypoints_index(t)
-        q1_index, v1_index, u1_index = self.get_keypoints_index(t+1)
-        q0 = traj[q0_index[0]:q0_index[-1]]
-        v0 = traj[v0_index[0]:v0_index[-1]]
-        u0 = traj[u0_index[0]:u0_index[-1]]
-        q1 = traj[q1_index[0]:q1_index[-1]]
-        v1 = traj[v1_index[0]:v1_index[-1]]
-        u1 = traj[u1_index[0]:u1_index[-1]]
-
-        x0 = traj[q0_index[0]:v0_index[-1]]
-        x1 = traj[q1_index[0]:v1_index[-1]]
-        # the deviavie respect to [q, v], not including u: x_dot = f(x, u)
-        #TODO: each point is evaluted twice; this can be improved
-        d0 = np.hstack([v0, self.dynamics(q0, v0, u0)])
-        d1 = np.hstack([v1, self.dynamics(q1, v1, u1)])
-
-        error = (x1 - x0) - 0.5*self.dt*(d0 + d1) #3.2 of Kelly(2017)
-        return error
-
-    def eval_g(self, traj):
-        residual = [self.dynamic_error(t, traj) for t in range(0, self.n-1)]
-        return np.hstack(residual)
-        # return np.sum(np.hstack(residual))
-
-
-    def eval_jac_g(self, X, flag):
+    def __call__(self, traj, flag):
         if flag:
-            return self.jac_index
-        point_jac_lst = [self.get_point_jac_value(X, t)
-                         for t in range(0, self.n-1)]
-        jac_flat = np.hstack(point_jac_lst)
-        return jac_flat
+            return self.position
+        return self.value(traj)
 
 
-def eval_f(X):
-    #this is the cost function
-    q_arr, v_arr, u_arr = X.reshape(-1, 3).T
-    square = np.power(u_arr, 2)
-    cost = np.sum(square)
-    return cost
+class Point_Dynamic_Hessian:
+    def __init__(self, problem, dynamic_hessian, t):
+        self.prob = prob
+        self.dynamic_hessian = dynamic_hessian
+        self.t = t
+        self.position = self.get_position()
+
+    def get_position(self):
+        pass
 
 
-def eval_g(X):
-    q_arr, v_arr, u_arr = X.reshape(-1, 3).T
+    def value(self, X):
+        pass
+
+    def __call__(traj, flag):
+        if flag:
+            return self.position
+        value = self.value(traj)
+        return value
+
+
+
+def traj_dynamic_error_function_factory(prob, dynamics):
+    #number of trajectory point
+    n = prob["n"]
+    #there is no dynamic constriant for the last point
+    func_lst = [Point_Dynamic_Error(prob, dynamics, t) for t in range(n-1)]
+    return func_lst
+
+
+def traj_dynamic_jacobian_function_factory(prob, dynamics_jac):
+    n = prob["n"]
+    #there is no dynamic constriant for the last point
+    func_lst =[Point_Dynamic_Jacobian(prob, dynamics_jac, t)
+               for t in range(n-1)]
+    return func_lst
+
+
+class Ipopt_Constriants:
+    def __init__(self, g_func_lst):
+        self.g_func_lst = g_func_lst
+
+    def __call__(self, X):
+        error_lst = [g(X) for g in self.g_func_lst]
+        error_arr = np.hstack(error_lst)
+        return error_arr
+
+
+class Ipopt_Constriants_Jacobian:
+    def __init__(self, g_jac_func_lst):
+        self.g_jac_func_lst = g_jac_func_lst
+        self.N = len(self.g_jac_func_lst)
+
+    def __call__(self, X, flag):
+        result_iter = (g_jac(X, flag) for g_jac in self.g_jac_func_lst)
+        if flag:# return positions, not values
+            col_pos_iter = result_iter
+            row_col_iter = it.chain.from_iterable(col_pos_iter)
+            pos_lst = [[(r, c) for c in cs] for (r, cs) in enumerate(row_col_iter)]
+            pos_arr = np.vstack(pos_lst)
+            row_arr = pos_arr[:, 0]
+            col_arr = pos_arr[:, 1]
+            return row_arr, col_arr
+        value_iter = result_iter
+        value_lst = list(value_iter)
+        value_arr = np.hstack(value_lst)
+        return value_arr
 
 
 if __name__=="__main__":
-    n = 20 # number of time point
-    dt = 1/n
-    start = (0, 0)
-    end = (1, 1)
+    prob = {}
+    prob["n"] = 20 # number of trajectory point
+    prob["dt"] = 1.0/prob["n"]
+    prob["start"] = (0, 0)
+    prob["end"] = (1, 1)
+    prob["qdim"] = 1
+    prob["udim"] = 1
+
+
+    start = prob["start"]
+    end = prob["end"]
+    n = prob['n']
     q_arr = np.linspace(start[0], end[0], n)
     v_arr = np.linspace(start[1], end[1], n)
     u_arr = np.zeros_like(q_arr, dtype=float)
     X_init = np.vstack([q_arr, v_arr, u_arr]).flatten("F")
 
-    constriant = Dynamic_constraints(1, 1, dt, n, block_dymamics, block_dymamics_jac)
-    g = constriant.eval_g(X_init)
-    result = constriant.eval_jac_g(X_init, False)
+    dynamic_g_lst = traj_dynamic_error_function_factory(prob, block_dymamics)
+    dynamic_g_jac_lst = traj_dynamic_jacobian_function_factory(prob, block_dymamics_jac)
+    ipopt_g = Ipopt_Constriants(dynamic_g_lst)
+    ipopt_g_jac = Ipopt_Constriants_Jacobian(dynamic_g_jac_lst)
+
+    jac_pos = ipopt_g_jac(X_init, True)
+    jac_value = ipopt_g_jac(X_init, False)
+
 
 
 
