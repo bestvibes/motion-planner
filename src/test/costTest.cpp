a12511cb6ce29534615c20e0a5db772f4d1e2b34
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2018-05-02 16:41:37 -0700

    cost: add square sum cost gradient + tests

--- src/test/costTest.cpp
+++ src/test/costTest.cpp
@@ -8,47 +8,47 @@ using namespace trajectoryOptimization::cost;
 using namespace trajectoryOptimization::utilities;
 
 TEST(costTest, controlSquare_isZero_whenControlsAreAllZero) { 
-	const unsigned numberOfPoints = 3;    
-	const unsigned pointDimension = 3;  
-	const unsigned controlDimension = 1; 
+	const unsigned numberOfPoints = 3;
+	const unsigned pointDimension = 3;
+	const unsigned controlDimension = 1;
 	const auto trajectoryDimension = numberOfPoints * pointDimension;
-	std::array<double, trajectoryDimension> trajectoryWithZeroControl;  
+	std::array<double, trajectoryDimension> trajectoryWithZeroControl;
 	const double * trajectoryWithZeroControl_RawDouble = trajectoryWithZeroControl.data();
 	trajectoryWithZeroControl.fill(0);
 
 	auto getControlSquareSum = GetControlSquareSum(numberOfPoints,
-																								 pointDimension,
-																								 controlDimension);
+													pointDimension,
+													controlDimension);
 	EXPECT_EQ(0, getControlSquareSum(trajectoryWithZeroControl_RawDouble));
 }
 
 
-TEST(costTest, controlSquareIsTwelveWhenContorlsAreThreeTwo) { 
+TEST(costTest, controlSquareIsTwelveWhenControlsAreThreeTwo) {
 	using namespace ranges;
-	const unsigned numberOfPoints = 3;    
-	const unsigned pointDimension = 3;  
-	const unsigned controlDimension = 1; 
+	const unsigned numberOfPoints = 3;
+	const unsigned pointDimension = 3;
+	const unsigned controlDimension = 1;
 	const auto trajectoryDimension = numberOfPoints * pointDimension;
 	std::vector<double> controlTwoPoint = {{0, 0, 2}};
 	auto trajectoryWithControlTwo = createTrajectoryWithIdenticalPoints(
 																	numberOfPoints,
-																	controlTwoPoint); 
+																	controlTwoPoint);
 	assert(trajectoryWithControlTwo.size() == trajectoryDimension);
 	const double* trajectoryWithControlTwo_rawDouble
 								= trajectoryWithControlTwo.data();
 
 
 	auto getControlSquareSum = GetControlSquareSum(numberOfPoints,
-																								 pointDimension,
-																								 controlDimension);
+													pointDimension,
+													controlDimension);
 	EXPECT_EQ(12, getControlSquareSum(trajectoryWithControlTwo_rawDouble));
 }
 
-TEST(costTest, controlSquareIs24WhenContorlsAreThreeTwoTwo) { 
+TEST(costTest, controlSquareIs24WhenControlsAreThreeTwoTwo) { 
 	using namespace ranges;
-	const unsigned numberOfPoints = 3;    
-	const unsigned pointDimension = 4;  
-	const unsigned controlDimension = 2; 
+	const unsigned numberOfPoints = 3;
+	const unsigned pointDimension = 4;
+	const unsigned controlDimension = 2;
 	const auto trajectoryDimension = numberOfPoints * pointDimension;
 	std::vector<double> controlTwoTwo = {{0, 0, 2, 2}};
 	auto trajectoryWithControlTwoTwo = createTrajectoryWithIdenticalPoints(
@@ -58,9 +58,77 @@ TEST(costTest, controlSquareIs24WhenContorlsAreThreeTwoTwo) {
 	const double* trajectoryWithControlTwoTwo_ptr
 								= trajectoryWithControlTwoTwo.data();
 	auto getControlSquareSum = GetControlSquareSum(numberOfPoints,
-																								 pointDimension,
-																								 controlDimension);
+													pointDimension,
+													controlDimension);
 	EXPECT_EQ(24, getControlSquareSum(trajectoryWithControlTwoTwo_ptr));
+}
+
+TEST(costTest, controlSquareGradient_isZero_whenControlsAreAllZero) { 
+	const unsigned numberOfPoints = 3;
+	const unsigned pointDimension = 3;
+	const unsigned controlDimension = 1;
+	const auto trajectoryDimension = numberOfPoints * pointDimension;
+	std::array<double, trajectoryDimension> trajectoryWithZeroControl;
+	const double * trajectoryWithZeroControl_RawDouble = trajectoryWithZeroControl.data();
+	trajectoryWithZeroControl.fill(0);
+
+	auto getControlSquareSumGradient = GetControlSquareSumGradient(numberOfPoints,
+													pointDimension,
+													controlDimension);
+
+	const std::vector<double> expected(trajectoryDimension);
+	EXPECT_EQ(expected, getControlSquareSumGradient(trajectoryWithZeroControl_RawDouble));
+}
+
+
+TEST(costTest, controlSquareGradientWhenControlsAreThreeTwo) {
+	using namespace ranges;
+	const unsigned numberOfPoints = 3;
+	const unsigned pointDimension = 3;
+	const unsigned controlDimension = 1;
+	const auto trajectoryDimension = numberOfPoints * pointDimension;
+	std::vector<double> controlTwoPoint = {{0, 0, 2}};
+	auto trajectoryWithControlTwo = createTrajectoryWithIdenticalPoints(
+																	numberOfPoints,
+																	controlTwoPoint);
+	assert(trajectoryWithControlTwo.size() == trajectoryDimension);
+	const double* trajectoryWithControlTwo_rawDouble
+								= trajectoryWithControlTwo.data();
+
+
+	const std::vector<double> expectedPoint = {0, 0, 4};
+	const std::vector<double> expected = createTrajectoryWithIdenticalPoints(
+																	numberOfPoints,
+																	expectedPoint);
+	auto getControlSquareSumGradient = GetControlSquareSumGradient(numberOfPoints,
+													pointDimension,
+													controlDimension);
+	EXPECT_EQ(expected, getControlSquareSumGradient(trajectoryWithControlTwo_rawDouble));
+}
+
+TEST(costTest, controlSquareGradientWhenControlsAreThreeTwoTwo) { 
+	using namespace ranges;
+	const unsigned numberOfPoints = 3;
+	const unsigned pointDimension = 4;
+	const unsigned controlDimension = 2;
+	const auto trajectoryDimension = numberOfPoints * pointDimension;
+	std::vector<double> controlTwoTwo = {{0, 0, 2, 2}};
+	auto trajectoryWithControlTwoTwo = createTrajectoryWithIdenticalPoints(
+																		 numberOfPoints,
+																		 controlTwoTwo);
+	assert(trajectoryWithControlTwoTwo.size() == trajectoryDimension);
+	const double* trajectoryWithControlTwoTwo_ptr
+								= trajectoryWithControlTwoTwo.data();
+
+
+	const std::vector<double> expectedPoint = {0, 0, 4, 4};
+	const std::vector<double> expected = createTrajectoryWithIdenticalPoints(
+																	numberOfPoints,
+																	expectedPoint);
+	auto getControlSquareSumGradient = GetControlSquareSumGradient(numberOfPoints,
+													pointDimension,
+													controlDimension);
+	EXPECT_EQ(expected, getControlSquareSumGradient(trajectoryWithControlTwoTwo_ptr));
 }
  
 int main(int argc, char **argv) {
--- src/trajectoryOptimizationLibrary/cost.hpp
+++ src/trajectoryOptimizationLibrary/cost.hpp
@@ -10,40 +10,82 @@ namespace trajectoryOptimization::cost{
 		const unsigned numberOfPoints;
 		const unsigned pointDimension;
 		const unsigned controlDimension;
-		int trajectoryDimension; 
-		int controlStartIndex; 
-		int controlEndIndex;
+		const int trajectoryDimension; 
+		const int controlStartIndex; 
+		const int controlEndIndex;
 		public:
-			GetControlSquareSum( const unsigned numberOfPoints,
-													 const unsigned pointDimension,
-													 const unsigned controlDimension):
-				numberOfPoints(numberOfPoints),
-				pointDimension(pointDimension),
-				controlDimension(controlDimension)
-		{
-				trajectoryDimension = numberOfPoints * pointDimension;
-				assert(controlDimension<pointDimension);
-				controlStartIndex = pointDimension - controlDimension;
-				controlEndIndex = pointDimension;
-			};
+			GetControlSquareSum(const unsigned numberOfPoints,
+								const unsigned pointDimension,
+								const unsigned controlDimension):
+									numberOfPoints(numberOfPoints),
+									pointDimension(pointDimension),
+									controlDimension(controlDimension),
+									trajectoryDimension(numberOfPoints * pointDimension),
+									controlStartIndex(pointDimension - controlDimension),
+									controlEndIndex(pointDimension)
+								{
+									assert(controlDimension<pointDimension);
+								};
+
 			double operator()(const double* trajectoryPointer) const {
-				using namespace ranges;
-			std::vector<double> trajectory(trajectoryPointer,
-																		 trajectoryPointer+trajectoryDimension);
+				std::vector<double> trajectory(trajectoryPointer,
+											 	trajectoryPointer+trajectoryDimension);
 
-			auto squareScaler =  [](auto singleControlValue)
-													 {return std::pow(singleControlValue, 2);};
+				auto squareScaler =  [](auto singleControlValue)
+										 {return std::pow(singleControlValue, 2);};
 
-			auto squareAPoint = [=](auto point) {
-				return point | ranges::view::slice(controlStartIndex, controlEndIndex) // get u
-										 | ranges::view::transform(squareScaler); //square
-			};
+				auto squareAPoint = [=](auto point) {
+					return point | ranges::view::slice(controlStartIndex, controlEndIndex) // get u
+								 | ranges::view::transform(squareScaler); //square
+				};
 
-			auto controlSquare = trajectory | view::chunk(pointDimension)
-																		  | view::transform(squareAPoint)
-																			| view::join;
-			double controlSquareSum = ranges::accumulate(controlSquare, 0);
-			return controlSquareSum;
+				auto controlSquare = trajectory | ranges::view::chunk(pointDimension)
+												  | ranges::view::transform(squareAPoint)
+													| ranges::view::join;
+				double controlSquareSum = ranges::accumulate(controlSquare, 0);
+				return controlSquareSum;
+			}  
+	};
+
+	class GetControlSquareSumGradient{
+		const unsigned numberOfPoints;
+		const unsigned pointDimension;
+		const unsigned controlDimension;
+		const int trajectoryDimension; 
+		const int controlStartIndex; 
+		const int controlEndIndex;
+		public:
+			GetControlSquareSumGradient(const unsigned numberOfPoints,
+										const unsigned pointDimension,
+										const unsigned controlDimension):
+											numberOfPoints(numberOfPoints),
+											pointDimension(pointDimension),
+											controlDimension(controlDimension),
+											trajectoryDimension(numberOfPoints * pointDimension),
+											controlStartIndex(pointDimension - controlDimension),
+											controlEndIndex(pointDimension)
+										{
+											assert(controlDimension<pointDimension);
+										};
+
+			std::vector<double> operator()(const double* trajectoryPointer) const {
+				std::vector<double> trajectory(trajectoryPointer,
+											 	trajectoryPointer+trajectoryDimension);
+
+				auto doubleControlScaler =  [](auto singleControlValue)
+										 {return 2 * singleControlValue;};
+
+				std::vector<double> timePointWithoutControlGradient(pointDimension - controlDimension);
+
+				auto doubleControl = [=](auto point) {
+					return ranges::view::concat(timePointWithoutControlGradient, point | ranges::view::slice(controlStartIndex, controlEndIndex) // get u
+								 															| ranges::view::transform(doubleControlScaler)); //square
+				};
+
+				auto controlDoubled = trajectory | ranges::view::chunk(pointDimension)
+												  | ranges::view::transform(doubleControl)
+													| ranges::view::join;
+				return controlDoubled;
 			}  
 	};
 }//namespace
